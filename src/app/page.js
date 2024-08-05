f5c3582e5b4149de57996425a62b21dc047fbe4d
Marcello C Bustamante <marcellusvii@hotmail.com>
2024-08-05 10:59:26 +0200

    add loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,28 @@
 import Results from " @/components/Results";
+import { resolve } from "styled-jsx/css";
 
 const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
+
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
   );
-   const data = await res.json();
-   if(!res.ok) {
-    throw new Error('Could not fetch data')
-   }
-   const results = data.results;
-// console.log(results);
 
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error("Could not fetch data");
+  }
+  const results = data.results;
+  // console.log(results);
 
-  return <div> 
-    <Results results={results}/>
-  </div>;
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
